e7f6ce23bf663093e5c6c2f29d549a130dac7361
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-05 19:11:34 +0200

    add environments

--- apps/services/models.py
+++ apps/services/models.py
@@ -1,1 +1,70 @@
-# Create your models here.
+from django.db import models
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-modified_at", "name"]
+
+
+class KindOfService(models.Model):
+    name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-modified_at", "name"]
+
+
+class Service(models.Model):
+    date = models.DateField(blank=False, null=True)
+    client = models.ForeignKey("Client", on_delete=models.CASCADE)
+    kind_of_service = models.ForeignKey("KindOfService", on_delete=models.CASCADE)
+    time = models.DurationField(blank=False, null=True)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.date
+
+    class Meta:
+        ordering = ["-date", "date"]
