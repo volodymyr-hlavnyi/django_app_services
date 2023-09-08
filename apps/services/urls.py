b3c5b15f09950d1f86855be5fc9c3b34985cbefb
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-08 19:11:17 +0200

    working version with Clients

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -6,7 +6,7 @@ app_name = "services"
 urlpatterns = [
     path("list/", views.ClientListView.as_view(), name="client_list"),
     #
-    # path("delete/", views.delete_contacts_view, name="contacts_delete"),
+    path("list_kind/", views.KindOfServiceListView.as_view(), name="kindofservice_list"),
     # path("delete/<int:pk>/", views.ContactDeleteView.as_view(), name="contacts_delete"),
     # #
     # path("update/<int:pk>/", views.ContactUpdateView.as_view(), name="contacts_update"),
@@ -14,6 +14,7 @@ urlpatterns = [
     # path("generate/", views.generate_contacts_view, name="contacts_generate"),
     # #
     path("create/", views.ClientsCreateView.as_view(), name="create_client"),
+    path("create_kind/", views.KindOfServiceCreateView.as_view(), name="kindofservice_create"),
     # path("createdetails/<int:pk>/", views.ContactDetailCreateView.as_view(), name="contacts_details_create"),
     # # path("createdetails/<int:pk>/", views.contact_info_detail_view, name="contacts_details_create"),
     # #
--- apps/services/views.py
+++ apps/services/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
-from apps.services.models import Client
+from apps.services.models import Client, KindOfService
 
 
 def home(request):
     return render(request, "services/home.html")
-
-
-# Create your views here.
 
 
 class ClientsCreateView(CreateView):
@@ -21,3 +18,14 @@ class ClientsCreateView(CreateView):
 class ClientListView(ListView):
     model = Client
     context_object_name = "client_list"
+
+
+class KindOfServiceListView(ListView):
+    model = KindOfService
+    context_object_name = "kindofservice_list"
+
+
+class KindOfServiceCreateView(CreateView):
+    model = KindOfService
+    fields = ("name",)
+    success_url = reverse_lazy("services:kindofservice_list")
