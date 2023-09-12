bf53c7a8ee815dd7af79713192b02ad91391dcbd
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-12 22:49:57 +0200

    working version with Clients

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = "services"
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("list/", views.ClientListView.as_view(), name="client_list"),
     path("create/", views.ClientsCreateView.as_view(), name="create_client"),
     path("edit/<int:client_id>/", views.client_edit, name="client_edit"),
     path("delete/<int:client_id>/", views.client_delete, name="client_delete"),
-    path("currency/", views.currency_view, name='currency_view'),
+    path("currency/", views.currency_view, name="currency_view"),
     path("list_kind/", views.KindOfServiceListView.as_view(), name="kindofservice_list"),
     path("create_kind/", views.KindOfServiceCreateView.as_view(), name="kindofservice_create"),
     path("edit_kind/<int:kind_id>/", views.kindofservice_edit, name="kindofservice_edit"),
