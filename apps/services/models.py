947a7816749f44419326c80fec9e014dc1caec88
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-26 19:38:59 +0200

    move to PostgreSQL + form with Login and Sign Up

--- apps/services/models.py
+++ apps/services/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Client(models.Model):
@@ -74,6 +75,14 @@ class CurrencyRate(models.Model):
     name = models.CharField(max_length=100)
     rate = models.DecimalField(max_digits=10, decimal_places=4)
     last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
