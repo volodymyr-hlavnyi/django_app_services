f9101009aea49482c6dd243fcdedc9412d37733f
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-29 22:44:12 +0200

    merge Dmytro additional

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -17,17 +17,8 @@ urlpatterns = [
     path("delete_kind/<int:kind_id>/", views.kindofservice_delete, name="kindofservice_delete"),
     path("list_service/", views.ServiceListView.as_view(), name="service_list"),
     path("create_service/", views.ServiceCreateView.as_view(), name="service_create"),
-    path("edit_service/<int:service_id>/", views.service_edit, name="service_edit"),
-    path("delete_service/<int:service_id>/", views.service_delete, name="service_delete"),
-    # Login/Logout
+    path("info/<int:client_id>/", views.client_info, name="client_info"),
     path("signup/", signup_view, name="signup"),
     path("login/", login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    # Actions
-    # path("actions/", views.ActionListView.as_view(), name="action_list"),
-    path("actions/", views.action_list, name="action_list"),
-    path("action_close/<int:action_id>", views.action_close, name="action_close"),
-    path("actions/<int:action_id>", views.action_delete, name="action_delete"),
-    # User (settings)
-    path("edit_userprofile/<int:user_id>", views.userprofile_edit, name="userprofile_edit"),
 ]
--- apps/services/views.py
+++ apps/services/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth import login, logout
 from .forms import SignupForm, LoginForm
 
 import requests
+import logging
 
 
 def home(request):
@@ -160,6 +161,27 @@ def kindofservice_edit(request, kind_id):
         form = KindOfServiceForm(instance=kind)
 
     return render(request, "services/kindofservice_edit.html", {"form": form, "kind": kind})
+
+
+def client_info(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+
+    services_for_client = Service.objects.filter(client=client)
+    service_kinds = [item.kind_of_service for item in services_for_client]
+
+    logger = logging.getLogger("django")
+    logger.info(f"services_for_client list is {service_kinds} for name {client.name} with {client.id}")
+
+    if request.method == "GET":
+        pass
+        # client.delete()
+        # return redirect("services:client_list")
+
+    return render(
+        request=request,
+        template_name="services/client_info.html",
+        context={"client": client, "service_kinds": service_kinds},
+    )
 
 
 def signup_view(request):
