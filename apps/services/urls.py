1e99312ef863831fc8cfb9dcf9e9769ee61911c3
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-29 22:44:12 +0200

    merge Dmytro additional

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("delete_kind/<int:kind_id>/", views.kindofservice_delete, name="kindofservice_delete"),
     path("list_service/", views.ServiceListView.as_view(), name="service_list"),
     path("create_service/", views.ServiceCreateView.as_view(), name="service_create"),
+    path("info/<int:client_id>/", views.client_info, name="client_info"),
     path("signup/", signup_view, name="signup"),
     path("login/", login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
