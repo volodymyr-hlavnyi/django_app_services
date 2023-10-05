71385a7d06b9fcb5c1da3abdc7a4c77c799c433f
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-10-06 01:05:37 +0200

    add Light / Dark theme for users profile

--- apps/services/userprofile/settings.py
+++ apps/services/userprofile/settings.py
@@ -2,5 +2,10 @@ from apps.services.models.userprofile import UserProfile
 
 
 def get_user_theme(user_id: int) -> str:
-    userprofile = UserProfile.objects.get(user_id=user_id)
-    return userprofile.visual_theme
+    if user_id is None:
+        thema = "light"
+    else:
+        userprofile = UserProfile.objects.get(user_id=user_id)
+        thema = userprofile.visual_theme
+
+    return thema
