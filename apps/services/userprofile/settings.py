6588a4ca0dd19b2473203194e2e2e89afd4e3106
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-10-06 00:48:55 +0200

    Models separate to different files

--- apps/services/userprofile/settings.py
+++ apps/services/userprofile/settings.py
@@ -2,10 +2,5 @@ from apps.services.models.userprofile import UserProfile
 
 
 def get_user_theme(user_id: int) -> str:
-    if user_id is None:
-        thema = "light"
-    else:
-        userprofile = UserProfile.objects.get(user_id=user_id)
-        thema = userprofile.visual_theme
-
-    return thema
+    userprofile = UserProfile.objects.get(user_id=user_id)
+    return userprofile.visual_theme
